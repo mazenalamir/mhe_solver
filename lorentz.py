d3f287d0019701d60f2175f6e4edba250d471fad
Mazen Alamir <57303836+mazenalamir@users.noreply.github.com>
2025-07-23 09:20:49 +0200

    Update lorentz.py
    add comments to the Lorentz

--- lorentz.py
+++ lorentz.py
@@ -1,12 +1,14 @@
 import numpy as np
 from casadi import vertcat
 
+# Definition of the parameters of the problem
 nx, n_par, nu, nc, ny = 3, 4, 2, 2, 4
 p_min = np.array([4, 0, 15, 0.0])
 p_max = np.array([6, 4, 30, 0.2])
 x_min = np.array([-10000]*nx)
 x_max = np.array([+10000]*nx)
 
+# The dynamics of the system 
 def ode_lorentz(x,u,p):
 
     xdot = vertcat(
@@ -16,14 +18,18 @@ def ode_lorentz(x,u,p):
     )
     return xdot
 
+# The measured output 
 def h_lorentz(x,u,p):
 
     y = vertcat(x[0], x[2], u)
     return y
 
+# How to extract u from the measurement vector
 def u_lorentz(y):
     return y[-2:]
 
+# The constraints function 
+# constraints are to be enforced instant-wise 
 def c_lorentz(x, u, p):
 
     c = vertcat(
